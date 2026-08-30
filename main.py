a7afd854074924e2976559b2bf3cc827f411f84f
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-30 00:07:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import models
+import utils
+import os
+import inserir_registros
+from database import criar_banco, CRIAR_ESTOQUE
 
 def main():
-    arquivo_entrada = "vendas.xlsx"
-    
-    models.processar_importacao(arquivo_entrada)
-    
-    print("--- CLIENTES PENDENTES ---")
-    print(models.obter_clientes_por_status("Pendente").to_string(index=False))
-    
-    print("\n--- CLIENTES PAGOS ---")
-    print(models.obter_clientes_por_status("Pago").to_string(index=False))
-    
-    models.exportar_pendentes()
-    models.exportar_pagos()
-    print("\nProcesso concluído e arquivos exportados com sucesso!")
+    criar_banco(CRIAR_ESTOQUE)
+
+    arquivo = "estoque_supermercado.xlsx"
+    if not os.path.exists(arquivo):
+        raise FileNotFoundError(f"O arquivo {arquivo} não foi encontrado")
+    else:
+        print(f"O arquivo {arquivo} foi encontrado.")
+
+    inserir_registros.inserir_excel_bd(arquivo)
+
+    utils.estoque_baixo()
+    utils.valor_total_estoque()
 
 if __name__ == "__main__":
     main()
